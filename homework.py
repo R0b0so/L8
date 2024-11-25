842785a3a120e3301bfde0af83e79a22b5e0f772
R0b0so <ethansagar222@gmail.com>
2024-11-24 18:20:30 -0800

    hello

--- homework.py
+++ homework.py
@@ -1,3 +1,4 @@
 Number = int(input("What is the number you are trying to power?"))
 Power = int(input("What is the power of your number? (n)"))
-for i in range(Power): print(Number*Number)
+i = 1
+print(Number**Power)
